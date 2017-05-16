e2e769bb6fafd42a05dccc34c564b6006409e8f2
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-05-16 12:55:21 -0400

    service installer

---
+++ src/Service/Installer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration.Install;
+
+namespace Service
+{
+    [RunInstaller(true)]
+    [DesignerCategory("Code")]
+    public class Installer : System.Configuration.Install.Installer
+    {
+
+        private IContainer _components = null;
+        private ServiceProcessInstaller _serviceProcessInstaller;
+        private ServiceInstaller _serviceInstaller;
+
+        public Installer()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _components = new Container();
+            _serviceProcessInstaller = new ServiceProcessInstaller();
+            _serviceInstaller = new ServiceInstaller();
+
+            _serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            _serviceProcessInstaller.AfterInstall += new InstallEventHandler(AfterInstalled);
+
+            _serviceInstaller.ServiceName = "bitwarden Directory Connector";
+            Installers.AddRange(new System.Configuration.Install.Installer[] { _serviceProcessInstaller, _serviceInstaller });
+        }
+
+        private void AfterInstalled(object sender, InstallEventArgs e)
+        {
+
+        }
+    }
+}
