24da4e91a5ea88d31fb694dd8b47dcefbba32e90
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-05-18 08:56:21 -0400

    force get groups if there are any users

--- src/Core/Services/LdapDirectoryService.cs
+++ src/Core/Services/LdapDirectoryService.cs
@@ -46,16 +46,16 @@ namespace Bit.Core.Services
                 throw new ApplicationException("No configuration for sync.");
             }
 
-            List<GroupEntry> groups = null;
-            if(SettingsService.Instance.Sync.SyncGroups)
-            {
-                groups = await GetGroupsAsync(force);
-            }
-
             List<UserEntry> users = null;
             if(SettingsService.Instance.Sync.SyncUsers)
             {
                 users = await GetUsersAsync(force);
+            }
+
+            List<GroupEntry> groups = null;
+            if(SettingsService.Instance.Sync.SyncGroups)
+            {
+                groups = await GetGroupsAsync(force || users.Any());
             }
 
             return new Tuple<List<GroupEntry>, List<UserEntry>>(groups, users);
