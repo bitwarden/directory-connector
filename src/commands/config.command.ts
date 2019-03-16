7cc941cc84880076640fd611295fd543188b1968
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-03-16 11:27:16 -0400

    base cli program and sync command

--- src/commands/config.command.ts
+++ src/commands/config.command.ts
@@ -1,12 +1,13 @@
 import * as program from 'commander';
 
 import { EnvironmentService } from 'jslib/abstractions/environment.service';
+import { I18nService } from 'jslib/abstractions/i18n.service';
 
 import { Response } from 'jslib/cli/models/response';
 import { MessageResponse } from 'jslib/cli/models/response/messageResponse';
 
 export class ConfigCommand {
-    constructor(private environmentService: EnvironmentService) { }
+    constructor(private environmentService: EnvironmentService, private i18nService: I18nService) { }
 
     async run(setting: string, value: string, cmd: program.Command): Promise<Response> {
         setting = setting.toLowerCase();
@@ -18,7 +19,7 @@ export class ConfigCommand {
                 return Response.badRequest('Unknown setting.');
         }
 
-        const res = new MessageResponse('Saved setting `' + setting + '`.', null);
+        const res = new MessageResponse(this.i18nService.t('savedSetting', setting), null);
         return Response.success(res);
     }
 
