c8593eb12a7ae20dc64a07dfed2199eb78742394
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-03-16 21:42:11 -0400

    appveyor.yml build

---
+++ make-versioninfo.ps1
@@ -0,0 +1,33 @@
+$major,$minor,$patch = $env:PACKAGE_VERSION.split('.')
+
+$versionInfo = @"
+
+1 VERSIONINFO
+FILEVERSION $major,$minor,$patch,0
+PRODUCTVERSION $major,$minor,$patch,0
+FILEOS 0x40004
+FILETYPE 0x1
+{
+BLOCK "StringFileInfo"
+{
+	BLOCK "040904b0"
+	{
+		VALUE "CompanyName", "8bit Solutions LLC"
+		VALUE "ProductName", "Bitwarden"
+		VALUE "FileDescription", "Bitwarden Directory Connector CLI"
+		VALUE "FileVersion", "$env:PACKAGE_VERSION"
+		VALUE "ProductVersion", "$env:PACKAGE_VERSION"
+		VALUE "OriginalFilename", "bwdc.exe"
+		VALUE "InternalName", "bwdc"
+		VALUE "LegalCopyright", "Copyright 8bit Solutions LLC"
+	}
+}
+
+BLOCK "VarFileInfo"
+{
+	VALUE "Translation", 0x0409 0x04B0  
+}
+}
+"@
+
+$versionInfo | Out-File ./version-info.rc
