a444294b340dcb2f1ac473cd0bbb1b617a27bb45
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-03-18 13:04:23 -0400

    clear cache command

---
+++ src/commands/clearCache.command.ts
@@ -0,0 +1,22 @@
+import * as program from 'commander';
+
+import { I18nService } from 'jslib/abstractions/i18n.service';
+
+import { ConfigurationService } from '../services/configuration.service';
+
+import { Response } from 'jslib/cli/models/response';
+import { MessageResponse } from 'jslib/cli/models/response/messageResponse';
+
+export class ClearCacheCommand {
+    constructor(private configurationService: ConfigurationService, private i18nService: I18nService) { }
+
+    async run(cmd: program.Command): Promise<Response> {
+        try {
+            await this.configurationService.clearStatefulSettings(true);
+            const res = new MessageResponse(this.i18nService.t('syncCacheCleared'), null);
+            return Response.success(res);
+        } catch (e) {
+            return Response.error(e);
+        }
+    }
+}
