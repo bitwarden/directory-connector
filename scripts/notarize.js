1df7283e5c228e1cbd432c3beee9452f8f172660
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-07-25 14:17:50 -0400

    notarize directory connector

---
+++ scripts/notarize.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+    const { electronPlatformName, appOutDir } = context;
+    if (electronPlatformName !== 'darwin') {
+        return;
+    }
+    const appleId = process.env.APPLEID;
+    const appName = context.packager.appInfo.productFilename;
+    return await notarize({
+        appBundleId: 'com.bitwarden.directory-connector',
+        appPath: `${appOutDir}/${appName}.app`,
+        appleId: appleId,
+        appleIdPassword: `@keychain:AC_PASSWORD`,
+    });
+};
