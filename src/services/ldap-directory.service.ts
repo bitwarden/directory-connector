369d48557b080f6cd1dd07938b2ef37909609539
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-05-19 10:27:40 -0400

    debug paged = false

--- src/services/ldap-directory.service.ts
+++ src/services/ldap-directory.service.ts
@@ -293,7 +293,7 @@ export class LdapDirectoryService implements DirectoryService {
         const options: ldap.SearchOptions = {
             filter: filter,
             scope: 'sub',
-            paged: true,
+            paged: false,
         };
         const entries: T[] = [];
         return new Promise<T[]>((resolve, reject) => {
