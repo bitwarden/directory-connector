20ef00648d3fe6630a17cc2d639b3f7694428c49
Joseph Flinn <joseph.s.flinn@gmail.com>
2021-01-29 17:47:04 +0000

    working on the mac packaging

--- .github/scripts/macos/decrypt-secrets.ps1
+++ .github/scripts/macos/decrypt-secrets.ps1
@@ -2,10 +2,7 @@
 
 $decryptSecretPath = $($rootPath + "/.github/scripts/decrypt-secret.ps1");
 
-Invoke-Expression "& `"$decryptSecretPath`" -filename bitwarden-connector-key.p12.gpg"
-Invoke-Expression "& `"$decryptSecretPath`" -filename appstore-app-cert.p12.gpg"
-Invoke-Expression "& `"$decryptSecretPath`" -filename appstore-installer-cert.p12.gpg"
+#Invoke-Expression "& `"$decryptSecretPath`" -filename bitwarden-connector-key.p12.gpg"
 Invoke-Expression "& `"$decryptSecretPath`" -filename devid-app-cert.p12.gpg"
 Invoke-Expression "& `"$decryptSecretPath`" -filename devid-installer-cert.p12.gpg"
 Invoke-Expression "& `"$decryptSecretPath`" -filename macdev-cert.p12.gpg"
-Invoke-Expression "& `"$decryptSecretPath`" -filename bitwarden_connector_appstore.provisionprofile.gpg"
