cac800be24a52aa1c0269a2292a4ec5e3f6c0552
Matt Gibson <mgibson@bitwarden.com>
2021-02-16 10:27:14 -0600

    Must ensure db file exists prior to initialization
    proper-lockfile throws if the file its locking does not exist

--- src/services/lowdbStorage.service.ts
+++ src/services/lowdbStorage.service.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as lock from 'proper-lockfile';
 
 import { LogService } from 'jslib/abstractions/log.service';
@@ -9,6 +10,16 @@ import { Utils } from 'jslib/misc/utils';
 export class LowdbStorageService extends LowdbStorageServiceBase {
     constructor(logService: LogService, defaults?: any, dir?: string, allowCache = false, private requireLock = false) {
         super(logService, defaults, dir, allowCache);
+    }
+
+    async init() {
+        if (!fs.existsSync(this.dataFilePath)) {
+            this.logService.warning(`Could not find data file, "${this.dataFilePath}"; creating it instead.`);
+            fs.writeFileSync(this.dataFilePath, '', { mode: 0o600 });
+            fs.chmodSync(this.dataFilePath, 0o600);
+            this.logService.info(`Created data file "${this.dataFilePath}" with chmod 600.`);
+        }
+        super.init();
     }
 
     protected async lockDbFile<T>(action: () => T): Promise<T> {
