83a877b38dde7d3eb410b22fbf1addaac3a8e2a7
Haneef <298208+haneef95@users.noreply.github.com>
2021-11-19 23:28:46 +0000

    Facilitate granular profile level variable declarations dynamically
    Each profile is executed in it's own process, as such, variables declared in a given profile won't affect other profiles.
    
    # Syntax: BWDC_CONF__<PROFILENAME>__<VARNAME>=<VALUE>
    # Example: BWDC_CONF__syncprofile_1__BW_CLIENTID=VALUE

--- dockerfiles/sync-profile.sh
+++ dockerfiles/sync-profile.sh
@@ -18,36 +18,39 @@ BWDC_PROFILENAME=${PWD##*/}
 # Allow users to specify conf per profile with Variables
 # Syntax: BWDC_CONF__<PROFILENAME>__<VARNAME>=<VALUE>
 # Example: BWDC_CONF__syncprofile_1__BW_CLIENTID=VALUE
-ProfileConfPrefix=${ProfileConfPrefix:-"BWDC_CONF__${BWDC_PROFILENAME}_"}
-
-tmp=${ProfileConfPrefix}_BW_CLIENTID
-[ -n "${!tmp}" ] && export BW_CLIENTID="${!tmp}"
-
-tmp=${ProfileConfPrefix}_BW_CLIENTSECRET
-[ -n "${!tmp}" ] && export BW_CLIENTSECRET="${!tmp}"
-# The last two lines can be repeated (ideally within a 'for each' syntax) to cater for more variables
+ProfileConfPrefix=${ProfileConfPrefix:-"BWDC_CONF__${BWDC_PROFILENAME}__"}
+eval 'vars=(${!'"$ProfileConfPrefix"'@})'
+echo "Found ${#vars[@]} environment variables to declare in this profile. Export below:"
+for i in "${vars[@]}"; do
+    ActualVarName=${i/"$ProfileConfPrefix"/}
+    # echo "$i corresponds to $ActualVarName"
+    echo "$ActualVarName=${!i}"
+    export $ActualVarName=${!i}
+done
 echo "Debug: All BW Variables in this session: ${!BW*}"
 
 # Print relevant info
-echo "bwdc config-file: $(bwdc config-file)"
-echo "bwdc last-sync groups: $(bwdc last-sync groups)"
+echo "bwdc config-file: $(bwdc data-file)"
 echo "bwdc last-sync users: $(bwdc last-sync users)"
+echo "bwdc last-sync groups: $(bwdc last-sync groups)"
 
 # Note:
 # if (timeNow-lastSync)>interval: bwdc sync
 ## Needs to be done. Preferably within the nodejs app as opposed to the wrappper.
 ## Ideally with a BWDC_SYNC_IGNORE_INTERVAL option, to force a sync.
 # Using $BWDC_SCAN_SLEEP_DUR in the parent process for now.
-currInterval=$(jq .syncConfig.interval data.json)
+
+#currInterval=$(jq .syncConfig.interval data.json)
 
 # Enforce minimumm interval between syncs per profile
-[ $BWDC_SYNC_INTERVAL_MIN -gt $currInterval ] && currInterval=$BWDC_SYNC_INTERVAL_MIN
+#[ $BWDC_SYNC_INTERVAL_MIN -gt $currInterval ] && currInterval=$BWDC_SYNC_INTERVAL_MIN
 
-echo "currInterval (ignored for now):" $currInterval
+#echo "currInterval (ignored for now):" $currInterval
 
 echo "Starting sync for: ${BWDC_PROFILENAME}"
 bwdc sync
 
 # Print relevant info
-echo "bwdc last-sync groups: " bwdc last-sync groups
-echo "bwdc last-sync users: " bwdc last-sync users
+echo "bwdc last-sync users: $(bwdc last-sync users)"
+echo "bwdc last-sync groups: $(bwdc last-sync groups)"
+
