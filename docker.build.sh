298483a8bb8a4176858f6d866ee65159e9162fe9
Haneef <298208+haneef95@users.noreply.github.com>
2021-11-19 19:03:36 +0000

    docker build and docker-compose.example

---
+++ docker.build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+BWDC_VERSION=2.9.8
+
+DOCKER_BUILDKIT=1 docker build . --label com.bitwarden.product="bitwarden" \ 
+ --no-cache --pull --build-arg BWDC_VERSION -t bitwarden/directory-connector:${BWDC_VERSION}-containerise_v1.1 # --squash
