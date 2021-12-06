9bec2aa2f07088c462bb678ac12e19d5256223a7
Oscar Hinton <oscar@oscarhinton.com>
2021-12-06 11:03:12 +0000

    BEEEP: Refactor services DI (#180)

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,7 +3,9 @@ import { ApiKeyService } from 'jslib-common/abstractions/apiKey.service';
 import { AppIdService } from 'jslib-common/abstractions/appId.service';
 import { CryptoService } from 'jslib-common/abstractions/crypto.service';
 import { CryptoFunctionService } from 'jslib-common/abstractions/cryptoFunction.service';
+import { EnvironmentService } from 'jslib-common/abstractions/environment.service';
 import { I18nService } from 'jslib-common/abstractions/i18n.service';
+import { KeyConnectorService } from 'jslib-common/abstractions/keyConnector.service';
 import { LogService } from 'jslib-common/abstractions/log.service';
 import { MessagingService } from 'jslib-common/abstractions/messaging.service';
 import { PlatformUtilsService } from 'jslib-common/abstractions/platformUtils.service';
@@ -24,9 +26,11 @@ export class AuthService extends AuthServiceBase {
         tokenService: TokenService, appIdService: AppIdService, i18nService: I18nService,
         platformUtilsService: PlatformUtilsService, messagingService: MessagingService,
         vaultTimeoutService: VaultTimeoutService, logService: LogService, private apiKeyService: ApiKeyService,
-        cryptoFunctionService: CryptoFunctionService, setCryptoKeys = true) {
+        cryptoFunctionService: CryptoFunctionService, environmentService: EnvironmentService,
+        keyConnectorService: KeyConnectorService) {
         super(cryptoService, apiService, userService, tokenService, appIdService, i18nService, platformUtilsService,
-            messagingService, vaultTimeoutService, logService, cryptoFunctionService, setCryptoKeys);
+            messagingService, vaultTimeoutService, logService, cryptoFunctionService, environmentService,
+            keyConnectorService, false);
     }
 
     async logInApiKey(clientId: string, clientSecret: string): Promise<AuthResult> {
