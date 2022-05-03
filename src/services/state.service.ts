f67f113fe1184fb818358c64a2b8d3027d0c6b92
Robyn MacCallum <robyntmaccallum@gmail.com>
2022-05-03 07:20:10 -0400

    [EC-176] Fix CLI errors caused by server URLs (#269)
    * Only get global environment urls
    
    * remove unnecessary await

--- src/services/state.service.ts
+++ src/services/state.service.ts
@@ -2,6 +2,7 @@ import { LogService } from "jslib-common/abstractions/log.service";
 import { StateMigrationService } from "jslib-common/abstractions/stateMigration.service";
 import { StorageService } from "jslib-common/abstractions/storage.service";
 import { StateFactory } from "jslib-common/factories/stateFactory";
+import { EnvironmentUrls } from "jslib-common/models/domain/environmentUrls";
 import { GlobalState } from "jslib-common/models/domain/globalState";
 import { StorageOptions } from "jslib-common/models/domain/storageOptions";
 import { StateService as BaseStateService } from "jslib-common/services/state.service";
@@ -581,5 +582,9 @@ export class StateService
       directoryConfigurations: account.directoryConfigurations,
     };
     return Object.assign(this.createAccount(), persistentAccountInformation);
+  }
+
+  async getEnvironmentUrls(options?: StorageOptions): Promise<EnvironmentUrls> {
+    return this.getGlobalEnvironmentUrls(options);
   }
 }
