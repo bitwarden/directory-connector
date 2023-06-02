65c085c241f30dd4b05dd9925186610a69bd5630
Matt Bishop <mbishop@bitwarden.com>
2023-06-02 11:42:01 -0400

    Angular 15 (#326)
    * Install Angular CLI
    
    * Core setup and cleanup
    
    * TypeScript and webpack updates
    
    * Angular 13
    
    * Add JS lib to Angular workspace
    
    * Do not use JS library with workspace
    
    * Angular 14
    
    * Angular 15
    
    * Code fixes
    
    * Couple package bumps
    
    * Restore angularCompilerOptions
    
    * Remove property reference to users inside group that didn't exist

--- jslib/angular/src/components/modal/modal-injector.ts
+++ jslib/angular/src/components/modal/modal-injector.ts
@@ -1,10 +1,18 @@
-import { InjectFlags, InjectionToken, Injector, Type } from "@angular/core";
+import { InjectFlags, InjectOptions, Injector, ProviderToken } from "@angular/core";
 
 export class ModalInjector implements Injector {
   constructor(private _parentInjector: Injector, private _additionalTokens: WeakMap<any, any>) {}
 
-  get<T>(token: Type<T> | InjectionToken<T>, notFoundValue?: T, flags?: InjectFlags): T;
-  get(token: any, notFoundValue?: any, flags?: any) {
+  get<T>(
+    token: ProviderToken<T>,
+    notFoundValue: undefined,
+    options: InjectOptions & { optional?: false }
+  ): T;
+  get<T>(token: ProviderToken<T>, notFoundValue: null, options: InjectOptions): T;
+  get<T>(token: ProviderToken<T>, notFoundValue?: T, options?: InjectOptions | InjectFlags): T;
+  get<T>(token: ProviderToken<T>, notFoundValue?: T, flags?: InjectFlags): T;
+  get(token: any, notFoundValue?: any): any;
+  get(token: any, notFoundValue?: any, flags?: any): any {
     return this._additionalTokens.get(token) ?? this._parentInjector.get<any>(token, notFoundValue);
   }
 }
