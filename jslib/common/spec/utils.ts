1cde9bccfc432b0554684cd115a5e922edf979d3
Hinton <hinton@users.noreply.github.com>
2023-12-12 10:30:53 +0100

    Update makeStaticByteArray

--- jslib/common/spec/utils.ts
+++ jslib/common/spec/utils.ts
@@ -28,10 +28,10 @@ export function mockEnc(s: string): EncString {
   return mock;
 }
 
-export function makeStaticByteArray(length: number) {
+export function makeStaticByteArray(length: number, start = 0) {
   const arr = new Uint8Array(length);
   for (let i = 0; i < length; i++) {
-    arr[i] = i;
+    arr[i] = start + i;
   }
   return arr;
 }
