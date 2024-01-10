8caedf289a9f67cb76e12781070dcc8e8f6b6d66
Addison Beck <hello@addisonbeck.com>
2024-01-10 13:50:49 -0600

    Delete unused utils tests

--- test.setup.ts
+++ test.setup.ts
@@ -1,7 +1,6 @@
 import { webcrypto } from "crypto";
 
 import "jest-preset-angular/setup-jest";
-import { toEqualBuffer } from "jslib/common/spec";
 
 Object.defineProperty(window, "CSS", { value: null });
 Object.defineProperty(window, "getComputedStyle", {
@@ -32,11 +31,3 @@ Object.defineProperty(window, "crypto", {
 Object.defineProperty(window, "crypto", {
   value: webcrypto,
 });
-
-expect.extend({
-  toEqualBuffer: toEqualBuffer,
-});
-
-export interface CustomMatchers<R = unknown> {
-  toEqualBuffer(expected: Uint8Array | ArrayBuffer): R;
-}
