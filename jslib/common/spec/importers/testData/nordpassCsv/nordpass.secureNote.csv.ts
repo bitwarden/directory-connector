63538588d1e4e5ae261ca22e7d9cf13b5e5f3d51
Daniel James Smith <djsmith85@users.noreply.github.com>
2024-01-17 12:32:18 +0100

    Remove all importers and related files

--- jslib/common/spec/importers/testData/nordpassCsv/nordpass.secureNote.csv.ts
+++
@@ -1,3 +0,0 @@
-export const data = `name,url,username,password,note,cardholdername,cardnumber,cvc,expirydate,zipcode,folder,full_name,phone_number,email,address1,address2,city,country,state
-notesFolder,,,,,,,,,,,,,,,,,,
-MySuperSecureNoteTitle,,,,MySuperSecureNote,,,,,,notesFolder,,,,,,,,`;
--- jslib/common/src/importers/importError.ts
+++
@@ -1,5 +0,0 @@
-export class ImportError extends Error {
-  constructor(message?: string, public passwordRequired: boolean = false) {
-    super(message);
-  }
-}
