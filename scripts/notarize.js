14314a3553b5b3c70ffcb7db0993fa0b9e638d13
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2024-08-26 11:49:15 -0400

    Switch to API key (#566)

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -7,15 +7,30 @@ exports.default = async function notarizing(context) {
   if (electronPlatformName !== "darwin") {
     return;
   }
-  const appleId = process.env.APPLE_ID_USERNAME || process.env.APPLEID;
-  const appleIdPassword = process.env.APPLE_ID_PASSWORD || `@keychain:AC_PASSWORD`;
+
   const appName = context.packager.appInfo.productFilename;
-  return await notarize({
-    tool: "notarytool",
-    appBundleId: "com.bitwarden.directory-connector",
-    appPath: `${appOutDir}/${appName}.app`,
-    teamId: "LTZ2PFU5D6",
-    appleId: appleId,
-    appleIdPassword: appleIdPassword,
-  });
+  if (process.env.APP_STORE_CONNECT_TEAM_ISSUER) {
+    const appleApiIssuer = process.env.APP_STORE_CONNECT_TEAM_ISSUER;
+    const appleApiKey = process.env.APP_STORE_CONNECT_AUTH_KEY_PATH;
+    const appleApiKeyId = process.env.APP_STORE_CONNECT_AUTH_KEY;
+    return await notarize({
+      tool: "notarytool",
+      appBundleId: "com.bitwarden.directory-connector",
+      appPath: `${appOutDir}/${appName}.app`,
+      appleApiIssuer: appleApiIssuer,
+      appleApiKey: appleApiKey,
+      appleApiKeyId: appleApiKeyId,
+    });
+  } else {
+    const appleId = process.env.APPLE_ID_USERNAME || process.env.APPLEID;
+    const appleIdPassword = process.env.APPLE_ID_PASSWORD || `@keychain:AC_PASSWORD`;
+    return await notarize({
+      tool: "notarytool",
+      appBundleId: "com.bitwarden.directory-connector",
+      appPath: `${appOutDir}/${appName}.app`,
+      teamId: "LTZ2PFU5D6",
+      appleId: appleId,
+      appleIdPassword: appleIdPassword,
+    });
+  }
 };
