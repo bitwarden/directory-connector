5e1979e3f5b1422bcaaa105f08d8c35873f495b0
Thomas Rittson <trittson@bitwarden.com>
2024-09-18 10:40:24 +1000

    Bootstrap tests

--- src/commands/login.command.spec.ts
+++ src/commands/login.command.spec.ts
@@ -1,4 +1,4 @@
-import { MockProxy } from "jest-mock-extended";
+import { mock, MockProxy } from "jest-mock-extended";
 
 import { ApiService } from "@/jslib/common/src/abstractions/api.service";
 import { AuthService } from "@/jslib/common/src/abstractions/auth.service";
@@ -12,7 +12,22 @@ import { PolicyService } from "@/jslib/common/src/abstractions/policy.service";
 import { StateService } from "@/jslib/common/src/abstractions/state.service";
 import { TwoFactorService } from "@/jslib/common/src/abstractions/twoFactor.service";
 
+import { AuthResult } from "../../jslib/common/src/models/domain/authResult";
+import { ApiLogInCredentials } from "../../jslib/common/src/models/domain/logInCredentials";
+
 import { LoginCommand } from "./login.command";
+
+const clientId = "test_client_id";
+const clientSecret = "test_client_secret";
+
+// Mock responses from the inquirer prompt
+// This combines both prompt results into a single object which is returned both times
+jest.mock("inquirer", () => ({
+  createPromptModule: () => () => ({
+    clientId,
+    clientSecret,
+  }),
+}));
 
 describe("LoginCommand", () => {
   let authService: MockProxy<AuthService>;
@@ -30,6 +45,22 @@ describe("LoginCommand", () => {
   let loginCommand: LoginCommand;
 
   beforeEach(() => {
+    // reset env variables
+    delete process.env.BW_CLIENTID;
+    delete process.env.BW_CLIENTSECRET;
+
+    authService = mock();
+    apiService = mock();
+    i18nService = mock();
+    environmentService = mock();
+    passwordGenerationService = mock();
+    cryptoFunctionService = mock();
+    platformUtilsService = mock();
+    stateService = mock();
+    cryptoService = mock();
+    policyService = mock();
+    twoFactorService = mock();
+
     loginCommand = new LoginCommand(
       authService,
       apiService,
@@ -46,7 +77,36 @@ describe("LoginCommand", () => {
     );
   });
 
-  it("runs", () => {
-    expect(loginCommand).not.toBeNull();
+  it("works with supplied api key", async () => {
+    process.env.BW_CLIENTID = clientId;
+    process.env.BW_CLIENTSECRET = clientSecret;
+
+    authService.logIn.mockResolvedValue(new AuthResult()); // logging in with api key does not set any flag on the authResult
+
+    const result = await loginCommand.run(null, null, { apikey: true });
+
+    expect(authService.logIn).toHaveBeenCalledWith(new ApiLogInCredentials(clientId, clientSecret));
+    expect(result).toMatchObject({
+      data: {
+        title: "You are logged in!",
+      },
+      success: true,
+    });
+  });
+
+  it("works with prompted api key", async () => {
+    // mock responses to the ClientId and ClientSecret input prompts
+
+    authService.logIn.mockResolvedValue(new AuthResult()); // logging in with api key does not set any flag on the authResult
+
+    const result = await loginCommand.run(null, null, { apikey: true });
+
+    expect(authService.logIn).toHaveBeenCalledWith(new ApiLogInCredentials(clientId, clientSecret));
+    expect(result).toMatchObject({
+      data: {
+        title: "You are logged in!",
+      },
+      success: true,
+    });
   });
 });
