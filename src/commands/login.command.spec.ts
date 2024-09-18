9dc497dd134b0cc871ca702e24e816f724d1aaf1
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2024-09-19 07:50:40 +1000

    [AC-3047] Refactor LoginCommand to only use organization api key login (#621)
    * Add tests
    
    * Remove unused code from LoginCommand and refactor
    
    * Remove unused services
    
    * Remove unused npm deps
    
    * Install missing type-fest dep

---
+++ src/commands/login.command.spec.ts
@@ -0,0 +1,66 @@
+import { mock, MockProxy } from "jest-mock-extended";
+
+import { AuthService } from "@/jslib/common/src/abstractions/auth.service";
+import { AuthResult } from "@/jslib/common/src/models/domain/authResult";
+import { ApiLogInCredentials } from "@/jslib/common/src/models/domain/logInCredentials";
+
+import { LoginCommand } from "./login.command";
+
+const clientId = "test_client_id";
+const clientSecret = "test_client_secret";
+
+// Mock responses from the inquirer prompt
+// This combines both prompt results into a single object which is returned both times
+jest.mock("inquirer", () => ({
+  createPromptModule: () => () => ({
+    clientId,
+    clientSecret,
+  }),
+}));
+
+describe("LoginCommand", () => {
+  let authService: MockProxy<AuthService>;
+
+  let loginCommand: LoginCommand;
+
+  beforeEach(() => {
+    // reset env variables
+    delete process.env.BW_CLIENTID;
+    delete process.env.BW_CLIENTSECRET;
+
+    authService = mock();
+
+    loginCommand = new LoginCommand(authService);
+  });
+
+  it("uses client id and secret stored in environment variables", async () => {
+    process.env.BW_CLIENTID = clientId;
+    process.env.BW_CLIENTSECRET = clientSecret;
+
+    authService.logIn.mockResolvedValue(new AuthResult()); // logging in with api key does not set any flag on the authResult
+
+    const result = await loginCommand.run();
+
+    expect(authService.logIn).toHaveBeenCalledWith(new ApiLogInCredentials(clientId, clientSecret));
+    expect(result).toMatchObject({
+      data: {
+        title: "You are logged in!",
+      },
+      success: true,
+    });
+  });
+
+  it("uses client id and secret prompted from the user", async () => {
+    authService.logIn.mockResolvedValue(new AuthResult()); // logging in with api key does not set any flag on the authResult
+
+    const result = await loginCommand.run();
+
+    expect(authService.logIn).toHaveBeenCalledWith(new ApiLogInCredentials(clientId, clientSecret));
+    expect(result).toMatchObject({
+      data: {
+        title: "You are logged in!",
+      },
+      success: true,
+    });
+  });
+});
