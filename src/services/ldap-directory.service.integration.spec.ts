5cdeef2c8089aee3691d32ab2288cb5aa2690800
Thomas Rittson <trittson@bitwarden.com>
2024-10-01 08:22:09 +1000

    Refactor test setup

--- src/services/ldap-directory.service.integration.spec.ts
+++ src/services/ldap-directory.service.integration.spec.ts
@@ -10,7 +10,6 @@ import { SyncConfiguration } from "../models/syncConfiguration";
 import { LdapDirectoryService } from "./ldap-directory.service";
 import { StateService } from "./state.service";
 
-
 describe("ldapDirectoryService", () => {
   let logService: MockProxy<LogService>;
   let i18nService: MockProxy<I18nService>;
@@ -30,50 +29,10 @@ describe("ldapDirectoryService", () => {
   });
 
   it("gets users without TLS", async () => {
-    const ldapConfiguration: LdapConfiguration = {
-      ssl: false,
-      startTls: false,
-      tlsCaPath: null,
-      sslAllowUnauthorized: false,
-      sslCertPath: null,
-      sslKeyPath: null,
-      sslCaPath: null,
-      hostname: "localhost",
-      port: 389,
-      domain: null,
-      rootPath: "dc=bitwarden,dc=com",
-      currentUser: false,
-      username: "cn=admin,dc=bitwarden,dc=com",
-      password: "admin",
-      ad: false,
-      pagedSearch: false,
-    };
-    stateService.getDirectory.calledWith(DirectoryType.Ldap).mockResolvedValue(ldapConfiguration);
-
-    const syncConfig: SyncConfiguration = {
-      users: true,
-      groups: false,
-      interval: 5,
-      userFilter: null,
-      groupFilter: null,
-      removeDisabled: false,
-      overwriteExisting: false,
-      largeImport: false,
-      // Ldap properties
-      groupObjectClass: "group",
-      userObjectClass: "person",
-      groupPath: null,
-      userPath: null,
-      groupNameAttribute: "name",
-      userEmailAttribute: "mail",
-      memberAttribute: "member",
-      useEmailPrefixSuffix: false,
-      emailPrefixAttribute: "sAMAccountName",
-      emailSuffix: null,
-      creationDateAttribute: "whenCreated",
-      revisionDateAttribute: "whenChanged",
-    };
-    stateService.getSync.mockResolvedValue(syncConfig);
+    stateService.getDirectory
+      .calledWith(DirectoryType.Ldap)
+      .mockResolvedValue(getLdapConfiguration());
+    stateService.getSync.mockResolvedValue(getSyncConfiguration());
 
     // Always return all users
     stateService.getLastUserSync.mockResolvedValue(null);
@@ -81,4 +40,55 @@ describe("ldapDirectoryService", () => {
     const result = await directoryService.getEntries(true, true);
     expect(result).toEqual([undefined, testUserEntries]);
   });
+});
+
+/**
+ * @returns a basic ldap configuration without TLS/SSL enabled. Can be overridden by passing in a partial configuration.
+ */
+const getLdapConfiguration = (config?: Partial<LdapConfiguration>): LdapConfiguration => ({
+  ssl: false,
+  startTls: false,
+  tlsCaPath: null,
+  sslAllowUnauthorized: false,
+  sslCertPath: null,
+  sslKeyPath: null,
+  sslCaPath: null,
+  hostname: "localhost",
+  port: 389,
+  domain: null,
+  rootPath: "dc=bitwarden,dc=com",
+  currentUser: false,
+  username: "cn=admin,dc=bitwarden,dc=com",
+  password: "admin",
+  ad: false,
+  pagedSearch: false,
+  ...(config ?? {}),
+});
+
+/**
+ * @returns a basic sync configuration. Can be overridden by passing in a partial configuration.
+ */
+const getSyncConfiguration = (config?: Partial<SyncConfiguration>): SyncConfiguration => ({
+  users: true,
+  groups: false,
+  interval: 5,
+  userFilter: null,
+  groupFilter: null,
+  removeDisabled: false,
+  overwriteExisting: false,
+  largeImport: false,
+  // Ldap properties
+  groupObjectClass: "group",
+  userObjectClass: "person",
+  groupPath: null,
+  userPath: null,
+  groupNameAttribute: "name",
+  userEmailAttribute: "mail",
+  memberAttribute: "member",
+  useEmailPrefixSuffix: false,
+  emailPrefixAttribute: "sAMAccountName",
+  emailSuffix: null,
+  creationDateAttribute: "whenCreated",
+  revisionDateAttribute: "whenChanged",
+  ...(config ?? {}),
 });
