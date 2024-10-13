d65f42684e8e472370b2fb71fabfd28f34ecfce3
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2024-10-14 08:17:00 +1000

    [PM-13008] Add ldap integration tests (#637)

---
+++ openldap/group-fixtures.ts
@@ -0,0 +1,40 @@
+import { Jsonify } from "type-fest";
+
+import { GroupEntry } from "../src/models/groupEntry";
+
+// These must match the ldap server seed data in directory.ldif
+const data: Jsonify<GroupEntry>[] = [
+  {
+    userMemberExternalIds: [
+      "cn=Loella Mak,ou=Payroll,dc=bitwarden,dc=com",
+      "cn=Painterson Miki,ou=Product Development,dc=bitwarden,dc=com",
+      "cn=Roland Dyke,ou=Human Resources,dc=bitwarden,dc=com",
+    ],
+    groupMemberReferenceIds: [],
+    users: [],
+    referenceId: "cn=Blue Team,dc=bitwarden,dc=com",
+    externalId: "cn=Blue Team,dc=bitwarden,dc=com",
+    name: "Blue Team",
+  },
+  {
+    userMemberExternalIds: [
+      "cn=Shiela Harada,ou=Peons,dc=bitwarden,dc=com",
+      "cn=Micaela Doud,ou=Janitorial,dc=bitwarden,dc=com",
+    ],
+    groupMemberReferenceIds: [],
+    users: [],
+    referenceId: "cn=Red Team,dc=bitwarden,dc=com",
+    externalId: "cn=Red Team,dc=bitwarden,dc=com",
+    name: "Red Team",
+  },
+  {
+    userMemberExternalIds: [],
+    groupMemberReferenceIds: [],
+    users: [],
+    referenceId: "cn=Cleaners,ou=Janitorial,dc=bitwarden,dc=com",
+    externalId: "cn=Cleaners,ou=Janitorial,dc=bitwarden,dc=com",
+    name: "Cleaners",
+  },
+];
+
+export const groupFixtures = data.map((g) => GroupEntry.fromJSON(g));
