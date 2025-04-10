08eaff7ba962197e713d358532f8d513fb319ab0
Thomas Rittson <trittson@bitwarden.com>
2025-04-10 19:46:16 +1000

    remove unused file

--- src/services/sync-request-options.ts
+++
@@ -1,9 +0,0 @@
-import { GroupEntry } from "../models/groupEntry";
-import { UserEntry } from "../models/userEntry";
-
-export interface SyncRequestOptions {
-  groups: GroupEntry;
-  users: UserEntry;
-  removeDisabled: boolean;
-  overwriteExisting: boolean;
-}
