87fa62b943e919bdf1286cadaba5e6b877223a63
jrmccannon <jmccannon@bitwarden.com>
2025-06-24 11:23:03 -0500

    Accidentally removed. Adding entra back in.

--- src/models/account.ts
+++ src/models/account.ts
@@ -29,6 +29,7 @@ export class ClientKeys {
 export class DirectoryConfigurations {
   ldap: LdapConfiguration;
   gsuite: GSuiteConfiguration;
+  entra: EntraIdConfiguration;
   // Azure Active Directory was renamed to Entra ID, but we've kept the old account property name
   // to be backwards compatible with existing configurations.
   azure: EntraIdConfiguration;
