c8f112df0594a7456d684c2fc9eccc469e9ca629
Brandon <btreston@bitwarden.com>
2025-07-21 16:23:04 -0400

    clean up

--- src/app/tabs/settings.component.ts
+++ src/app/tabs/settings.component.ts
@@ -122,7 +122,7 @@ export class SettingsComponent implements OnInit, OnDestroy {
       return;
     }
 
-    (this.ldap as any)[id] = filePicker.files[0];
+    (this.ldap as any)[id] = filePicker.files[0].path;
     // reset file input
     // ref: https://stackoverflow.com/a/20552042
     filePicker.type = "";
