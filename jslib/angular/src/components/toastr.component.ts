45bc60527f305e0f035b5b61b032d227ce98a7da
Brandon <btreston@bitwarden.com>
2026-01-13 11:16:07 -0500

    ng update 21 wip

--- jslib/angular/src/components/toastr.component.ts
+++ jslib/angular/src/components/toastr.component.ts
@@ -13,43 +13,52 @@ import {
 @Component({
   selector: "[toast-component2]",
   template: `
-    <button
-      *ngIf="options.closeButton"
-      (click)="remove()"
-      type="button"
-      class="toast-close-button"
-      aria-label="Close"
-    >
-      <span aria-hidden="true">&times;</span>
-    </button>
+    @if (options.closeButton) {
+      <button
+        (click)="remove()"
+        type="button"
+        class="toast-close-button"
+        aria-label="Close"
+        >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    }
     <div class="icon">
       <i></i>
     </div>
     <div>
-      <div *ngIf="title" [class]="options.titleClass" [attr.aria-label]="title">
-        {{ title }} <ng-container *ngIf="duplicatesCount">[{{ duplicatesCount + 1 }}]</ng-container>
+      @if (title) {
+        <div [class]="options.titleClass" [attr.aria-label]="title">
+          {{ title }} @if (duplicatesCount) {
+          [{{ duplicatesCount + 1 }}]
+        }
       </div>
+    }
+    @if (message && options.enableHtml) {
       <div
-        *ngIf="message && options.enableHtml"
         role="alertdialog"
         aria-live="polite"
         [class]="options.messageClass"
         [innerHTML]="message"
       ></div>
+    }
+    @if (message && !options.enableHtml) {
       <div
-        *ngIf="message && !options.enableHtml"
         role="alertdialog"
         aria-live="polite"
         [class]="options.messageClass"
         [attr.aria-label]="message"
-      >
+        >
         {{ message }}
       </div>
+    }
     </div>
-    <div *ngIf="options.progressBar">
-      <div class="toast-progress" [style.width]="width + '%'"></div>
-    </div>
-  `,
+    @if (options.progressBar) {
+      <div>
+        <div class="toast-progress" [style.width]="width + '%'"></div>
+      </div>
+    }
+    `,
   animations: [
     trigger("flyInOut", [
       state("inactive", style({ opacity: 0 })),
