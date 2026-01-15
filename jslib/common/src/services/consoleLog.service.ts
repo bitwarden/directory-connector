71fe509a00df317a468fc8563066a61b705aca5b
Brandon <btreston@bitwarden.com>
2026-01-15 12:45:02 -0500

    fix import

--- jslib/common/src/services/consoleLog.service.ts
+++ jslib/common/src/services/consoleLog.service.ts
@@ -1,4 +1,4 @@
-import hrtime from "browser-hrtime";
+import * as hrtime from "browser-hrtime";
 
 import { LogService as LogServiceAbstraction } from "../abstractions/log.service";
 import { LogLevelType } from "../enums/logLevelType";
